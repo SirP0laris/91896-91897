3d135bc261f2c74af7dfe4d50f3b527200d4738e
SirP0laris <yuolivia0@gmail.com>
2026-07-22 12:07:11 +1200

    A new window size
    10/06/26

---
+++ activity_1_Window_size_v0.00.01.py
@@ -0,0 +1,10 @@
+# Author: mm no
+# Date: June 3rd 2026
+# Purpose: Create a greeting app
+
+import tkinter as tk
+from tkinter import ttk
+root = tk.Tk()
+root.minsize(width=400, height=400)
+root.resizable(width = False, height = False)
+root.mainloop()
